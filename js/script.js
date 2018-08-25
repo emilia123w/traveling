5e8240c0c1b3eefa1c43dbb73ee24b9cc74c7535
Emilia Walczak <emilia.walczak.87@wp.pl>
2018-08-25 19:18:44 +0200

    adding-functions

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function addAbout(){
+  closeMap();
+  document.getElementById('about').style.display="block";
+}
+
+function closeAbout(){
+  document.getElementById('about').style.display="none";
+}
+
+function displayPhotos(){
+  closeMap();
+  closeAbout();
+}
